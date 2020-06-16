e839a9b64d46b70b27876569812d7b9a4723433a
Nghĩa Trương <truongnghiahtn@gmail.com>
2020-06-16 15:18:29 +0700

    ok

---
+++ Source_Basketball_AR/Assets/PointCounter.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PointCounter : MonoBehaviour
+{
+    public GameObject scoreText;
+    public GameObject highScoreText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        scoreText.GetComponent<Text>().text = "0";
+        highScoreText.GetComponent<Text>().text = PlayerPrefs.GetInt("highScore").ToString();
+    }
+
+    void OnTriggerEnter() //if ball hits basket collider
+    {
+        int currentScore = int.Parse(scoreText.GetComponent<Text>().text) + 1; //add 1 to the score
+        int highScore = PlayerPrefs.GetInt("highScore");
+        if (currentScore > highScore)
+        {
+            PlayerPrefs.SetInt("highScore", currentScore);
+            highScoreText.GetComponent<Text>().text = currentScore.ToString();
+        }
+        scoreText.GetComponent<Text>().text = currentScore.ToString();
+    }
+}
---
+++ Source_Basketball_AR/Assets/ThrowBallBehavior.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThrowBallBehavior : MonoBehaviour
+{
+    private Rigidbody rb;
+    public float force = 2f;
+    public bool isTarget = false;
+    public float zFactor = 2f;
+    private GameObject parent;
+
+    public Vector3 startPosition;
+
+    private Vector2 startSwipe;
+    private Vector2 endSwipe;
+
+    private float startTime;
+    private float endTime;
+
+    public GameObject ballObject;
+
+    // Use this for initialization
+    void Start()
+    {
+        startPosition = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+        rb = GetComponent<Rigidbody>();
+        parent = transform.parent.gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            startTime = Time.time;
+            startSwipe = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            endTime = Time.time;
+            endSwipe = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+            if (isTarget == true)
+            {
+                rb.isKinematic = false;
+                Swipe();
+                isTarget = false;
+            }
+        }
+    }
+
+    void Swipe()
+    {
+        Destroy(gameObject, 5);
+        Vector3 swipe = endSwipe - startSwipe;
+        swipe.z = swipe.y / zFactor;
+        //rb.AddForce(swipe * force, ForceMode.Impulse);
+        rb.AddRelativeForce(new Vector3(0, 1.7f, 1) * force * (endTime - startTime), ForceMode.Impulse);
+        Invoke("SpawnNewBall", 2);
+    }
+
+    void SpawnNewBall()
+    {
+        GameObject newBall = Instantiate(ballObject);
+        newBall.transform.parent = parent.transform;
+        newBall.transform.localPosition = new Vector3(0, -0.2f, 0.8f);
+        newBall.transform.localRotation = Quaternion.Euler(0, 0, 0);
+        newBall.GetComponent<Rigidbody>().isKinematic = true;
+    }
+
+    private void OnMouseDown()
+    {
+        rb.constraints = RigidbodyConstraints.None;
+        isTarget = true;
+    }
+}
